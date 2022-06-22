3e8826cf94f6d5ffb3dd02ac4fc9802de65ff8e6
PaulHaze <dev@artemist.com.au>
2022-06-23 08:04:51 +1000

    removed old styles, updated index page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/main.scss';
+import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function SplititApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default SplititApp;
